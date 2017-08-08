e7d159ce2953a07082fc705f918857efb50268d5
Nathan Schulzke <nschulzke@gmail.com>
2017-08-08 14:30:22 +0000

    Switch to array map

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -3,6 +3,14 @@ class GameController < ApplicationController
     @players = Player.all;
     @monsters = Monster.all;
     @player = Player.find(params[:id])
+    
+    @map = Array.new(11){ Array.new(11) }
+    @players.each do |player|
+      @map[player.y_pos][player.x_pos] = "player"
+    end
+    @monsters.each do |monster|
+      @map[monster.y_pos][monster.x_pos] = "monster"
+    end
   end
 
   def menu
